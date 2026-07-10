05a1d8996eb16b5f1e5cf844588774ce0a9fc50a
AURYSS <ceciaurora2017@gmail.com>
2026-07-09 23:47:05 -0600

    feat(security): validacion de datos

--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -67,5 +67,6 @@ const router = Router();
  */
 router.post("/login", authController.login);
 router.post("/register", authController.register);
+router.post("/logout", authController.logout);
 
 export default router;
